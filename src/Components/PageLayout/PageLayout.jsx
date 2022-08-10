523e210ab3938657aab3e830b936d08a0a398679
shrutidhungana <shrutidhungana@prixa.net>
2022-08-10 12:59:47 +0545

    Completed PageLayout as well

--- src/Components/PageLayout/PageLayout.jsx
+++ src/Components/PageLayout/PageLayout.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ThemeContext } from "../../Context/ThemeContext";
 
 const PageLayout = () => {
-    
+  const { theme } = useContext(ThemeContext);
   return (
-      <div>
-          <header>HEADER</header>
-          <main>TOGGLING THEME USING USE CONTEXT</main>
-          <footer>FOOTER</footer>
-      </div>
-  )
-}
+    <div style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+      <header>HEADER</header>
+      <main>TOGGLING THEME USING USE CONTEXT</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
+};
 
-export default PageLayout
+export default PageLayout;
