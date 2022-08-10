3f826c8b593b83578a6b8ea1c7b059362768a088
shrutidhungana <shrutidhungana@prixa.net>
2022-08-10 12:45:29 +0545

    Finished creating a global context

--- src/Components/Button/Index.jsx
+++ src/Components/Button/Index.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {BsSun,BsSunFill} from 'react-icons/bs'
+import { ThemeContext } from '../../Context/ThemeContext'
 
 const Index = () => {
+    const {handleToggleIcon, isDarkTheme} = useContext(ThemeContext)
   return (
       <div>
           <button>Toggle theme</button>
--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,36 +1,45 @@
-import React, {createContext, useState} from 'react'
-
-
+import React, { createContext, useState, useEffect } from "react";
 
 const themes = {
-    dark: {
-        backgroundColor: "#000000",
-        color: "#ffffff"
-    },
-    light: {
-        backgroundColor: "#ffffff",
-        color: "#000000"
-    },
-}
+  dark: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+  },
+  light: {
+    backgroundColor: "#ffffff",
+    color: "#000000",
+  },
+};
 
 const initialState = {
-    theme: themes.light,
-    handleToggleIcon: () => { },
-    isDark: false
-}
-
+  theme: themes.light,
+  handleToggleIcon: () => {},
+  isDark: false,
+};
 
 const ThemeContext = createContext(initialState);
 
 const ThemeProvider = ({ children }) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false)
-    
-    const handleToggleIcon = () =>{}
-    return (
-        <ThemeContext.Provider value = {{themes,handleToggleIcon,isDark}}>
-           {children} 
-        </ThemeContext.Provider>
-    )
-}
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-export default ThemeProvider
+  useEffect(() => {
+    const extractDarkTheme = JSON.parse(localStorage.getItem("isDarkTheme"));
+    setIsDarkTheme(extractDarkTheme !== null ?extractDarkTheme : false);
+  }, []);
+
+  const handleToggleIcon = () => {
+    const updateIsDarkTheme = !isDarkTheme;
+    localStorage.setItem("isDarkTheme", JSON.stringify(updateIsDarkTheme));
+    setIsDarkTheme(updateIsDarkTheme);
+  };
+
+  const theme = isDarkTheme ? themes.dark : themes.light;
+
+  return (
+    <ThemeContext.Provider value={{ theme, handleToggleIcon, isDarkTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
